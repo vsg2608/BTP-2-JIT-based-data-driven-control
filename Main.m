84d4f02f2a301b822778782d15c131b72dc8c948
Vishesh Goyal <vsg2608@gmail.com>
2019-03-29 01:23:18 +0530

    System Identification

--- Main.m
+++ Main.m
@@ -3,12 +3,11 @@ clear;
 load ("./data/batch_norm_data.mat");
 
 qBatch= 5;
-qTime= 21;
+qTime= 31;
 qPoint= Data(qTime,:,qBatch);
-size_Profile=20;
+size_Profile=30;
 i_qTime=qTime-size_Profile+1;
 qProfile= Data(i_qTime:qTime,:,qBatch);
-X=[];
 U=[];
 W=[];
 Y=[];
@@ -16,13 +15,15 @@ for b=1:1
     tProfile= Data(:,:,b);
     [rProfile,totalCost,iTime]= TWED(qProfile,tProfile);
     for i= 1:size_Profile
-        X=vertcat(X, rProfile(i,:));
-        U=vertcat(U, rProfile(i,11));
-        Y=vertcat(Y, rProfile(i,13));
+        U=vertcat(U, rProfile(i,[1,2]));
+        Y=vertcat(Y, rProfile(i,3));
     end
 end
 
-
+data = iddata(Y,U,5);
+sys = ssest(data,2);
+yp = predict(sys,data,20);
+plot(yp);
 
 
 
